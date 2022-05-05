dd35e7cf011787987cee321d67970825a412f5cd
ChefuRM <reyes.edujm@gmail.com>
2022-05-04 22:50:40 -0600

    added class Empleados

--- Clases/cClientes.java
+++ Clases/cClientes.java
@@ -9,5 +9,32 @@ package Clases;
  * @author Chefu
  */
 public class cClientes {
-    
+
+    public String getNitCliente() {
+        return NitCliente;
+    }
+
+    public void setNitCliente(String NitCliente) {
+        this.NitCliente = NitCliente;
+    }
+
+    public String getNombreCliente() {
+        return NombreCliente;
+    }
+
+    public void setNombreCliente(String NombreCliente) {
+        this.NombreCliente = NombreCliente;
+    }
+
+    public String getDireccionCliente() {
+        return DireccionCliente;
+    }
+
+    public void setDireccionCliente(String DireccionCliente) {
+        this.DireccionCliente = DireccionCliente;
+    }
+    private String NitCliente;
+    private String NombreCliente;
+    private String DireccionCliente;
+
 }
---
+++ Clases/cEmpleados.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Chefu
+ */
+public class cEmpleados {
+
+    public String getCodigoEmpleado() {
+        return CodigoEmpleado;
+    }
+
+    public void setCodigoEmpleado(String CodigoEmpleado) {
+        this.CodigoEmpleado = CodigoEmpleado;
+    }
+
+    public String getNombreEmpleado() {
+        return NombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String NombreEmpleado) {
+        this.NombreEmpleado = NombreEmpleado;
+    }
+
+    public String getPuestoEmpleado() {
+        return PuestoEmpleado;
+    }
+
+    public void setPuestoEmpleado(String PuestoEmpleado) {
+        this.PuestoEmpleado = PuestoEmpleado;
+    }
+
+    public String getSalarioEmpleado() {
+        return SalarioEmpleado;
+    }
+
+    public void setSalarioEmpleado(String SalarioEmpleado) {
+        this.SalarioEmpleado = SalarioEmpleado;
+    }
+
+    private String CodigoEmpleado;
+    private String NombreEmpleado;
+    private String PuestoEmpleado;
+    private String SalarioEmpleado;
+
+}
